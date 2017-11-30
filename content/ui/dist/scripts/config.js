9229ee30999564d93d5f4652c5457550a490183e
Gabriel Rodrigues Couto <gabrielrcouto@gmail.com>
2017-11-30 10:41:52 -0200

    Initial commit

---
+++ content/ui/dist/scripts/config.js
@@ -0,0 +1,17 @@
+angular.module('config', [])
+
+.constant('supportEmail', '')
+
+.constant('authnType', 'basic')
+
+.constant('noAuthRedirect', '')
+
+.constant('ssoLogoutRedirect', '')
+
+.constant('apiAuthInfo', '')
+
+.constant('apiHostBaseUrlValue', 'DEFAULT')
+
+.constant('downloadBaseUrlValue', 'DEFAULT')
+
+;
